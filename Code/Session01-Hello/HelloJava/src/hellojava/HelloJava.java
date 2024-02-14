c407514311c58fcb4c28e3c304ae2fb494e7136a
locnht-it <locnht.it@gmail.com>
2024-02-14 11:04:24 +0700

    Create the session 01 - Hello
    The first project in the journey to learn about the object-oriented world.

---
+++ Code/Session01-Hello/HelloJava/src/hellojava/HelloJava.java
@@ -0,0 +1,19 @@
+
+package hellojava;
+
+public class HelloJava {
+
+    //Entry point, cửa chính, nơi cuộc chơi bắt đầu, CPU vào đây chạy lệnh.
+    public static void main(String[] args) {
+        System.out.println("Xin chào Java. \nWelcome to Java World.");
+        
+        System.out.println("5 + 5 = " + 5 + 5);
+        
+        System.out.println("5 + 5 = " + (5 + 5));
+        
+        //sout tab
+        //~ 100% printf("Xin chao Java...\n");
+        //F11, bên này F6 dịch, sau đó chạy app xem kết quả.
+    }
+    
+}
